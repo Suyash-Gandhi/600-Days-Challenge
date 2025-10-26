1884bc5afe9fb1ed34036f6d24dec2e9e2f94bc0
Suyash-Gandhi <suyash25gandhi@gmail.com>
2025-10-27 00:42:05 +0530

    initial commit

--- javascript/test.js
+++ javascript/test.js
@@ -1,6 +1,9 @@
-const a=()=>{
-console.log("acvbb");
-
-}
-
-a()
+{
+    let r;              // -------+-- 'a starts here
+    {                   //        |
+        let x = 5;      // ---+-- 'b starts here
+        r = x;         //    |   Error! 'b shorter than 'a
+    }                   // ---+-- 'b ends here
+    console.log(r);
+      // Dangling reference if allowed
+}                       // -------+-- 'a ends here
