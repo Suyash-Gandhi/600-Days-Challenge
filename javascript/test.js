8b15bf6b7ddb5f5bfffbf0718bd36d841dbedeb4
Suyash-Gandhi <suyash25gandhi@gmail.com>
2025-11-30 04:37:30 +0530

    initial commit

--- javascript/test.js
+++ javascript/test.js
@@ -1,8 +1,7 @@
-function greet(name = "Guest", ...messages) {
-  console.log(`Hello, ${name}!`);
-  console.log("Your messages:");
-  messages.forEach(msg => console.log(`- ${msg}`));
-}
-
-// Example calls
-greet("Suyash", "Welcome back!", "You have 3 new notifications.", "Have a great day!");
+const map = new Map([
+  ["name", "Suyash"],
+  ["age", 22]
+]);
+map.set("country", "India");
+map.set({ id: 1 }, "UserData");
+console.log(map);
