2966a664ce520701b4527c5f9f690f49b37e6381
Suyash-Gandhi <suyash25gandhi@gmail.com>
2026-05-27 21:48:41 +0530

    initial commit

--- DSA ( C )/test.c
+++ DSA ( C )/test.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
 
-int main(){
-    int arr[10]={1,-2,0.3,4.444444444444444444444444444444444444444444444444444444,5,6};
-     
-    for (int i = 0; i < 10; i++)
-    {
-        printf("%d ", arr[i]);
+int main() {
+    int arr[] = {10, 20, 30, 40, 50, 60};
+    int n = 6;
+    int target = 40;
+
+    int low = 0;
+    int high = n - 1;
+    int mid;
+
+    while(low <= high) {
+        mid = (low + high) / 2;
+
+        if(arr[mid] == target) {
+            printf("Element found at index %d", mid);
+            return 0;
+        }
+        else if(target < arr[mid]) {
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1;
+        }
     }
-    
+
+    printf("Element not found");
+
+    return 0;
 }
